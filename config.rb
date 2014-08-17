ad38945ca749b53f90ff52ea514d47d90d47c3fa
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-08-16 20:46:16 -0700

    removed name from site, replaced with generic link names

--- config.rb
+++ config.rb
@@ -40,7 +40,7 @@ set :slim, :layout_engine => :slim
 # page "/path/to/file.html", :layout => false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+page "/*", :layout => :trunk
 #
 # A path which all have the same layout
 # with_layout :admin do
@@ -50,7 +50,6 @@ set :slim, :layout_engine => :slim
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-
 
 
 
@@ -73,7 +72,7 @@ end
 #   end
 # end
 
-set :css_dir, 'assets/css/'
+set :css_dir, 'css'
 
 set :js_dir, 'js'
 
